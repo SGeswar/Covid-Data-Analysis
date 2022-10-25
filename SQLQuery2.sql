4e2e30743026efee2599fc1b371c7ebc11f20b44
SGeswar <110022221+SGeswar@users.noreply.github.com>
2022-10-26 03:00:09 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,6 +1,6 @@
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
-where (continent is not null and (continent = 'North America' or continent = 'Asia' or continent = 'South America' or continent = 'European Union' or continent = 'Africa' or continent = 'Oceania'))
+where (continent is not null 
 order by 1,2
 
 
